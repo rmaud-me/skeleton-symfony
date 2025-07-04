135218190171a25ff75b571f2b0be1a8a96aa94b
Maud @mihani <mihani@rmaud.me>
2025-07-04 17:55:13 +0200

    fix(twig): Fix load of twig bundle

--- config/bundles.php
+++ config/bundles.php
@@ -6,6 +6,6 @@ return [
     Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
     Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true],
     Symfony\Bundle\DebugBundle\DebugBundle::class => ['dev' => true],
-    Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
+    Symfony\Bundle\TwigBundle\TwigBundle::class => ['dev' => true, 'test' => true],
     Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
 ];
